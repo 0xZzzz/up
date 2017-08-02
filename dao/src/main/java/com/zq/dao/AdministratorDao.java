f5046f09ebc64bd627638931132c33e79e9ca77b
0xZzzz <69953213@qq.com>
2017-08-02 22:40:24 +0800

    fix

--- dao/src/main/java/com/zq/dao/AdministratorDao.java
+++ dao/src/main/java/com/zq/dao/AdministratorDao.java
@@ -9,17 +9,11 @@ public interface AdministratorDao {
 
     /**
      * 根据管理员id获取管理员信息
-     *
-     * @param id 管理员id
-     * @return 管理员信息
      */
     Administrator getById(long id);
 
     /**
      * 主键更新
-     *
-     * @param administrator 管理员信息
-     * @return effect rows
      */
     int update(Administrator administrator);
 
