e913625851e632edcdceec55d9dc234be3e88b06
0xZzzz <69953213@qq.com>
2017-08-05 13:14:26 +0800

    fix

---
+++ sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `administrator` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `username` varchar(20) NOT NULL COMMENT '用户名',
+  `password` varchar(20) NOT NULL COMMENT '密码',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `modify_time` datetime NOT NULL COMMENT '修改时间',
+  `status` tinyint(3) unsigned NOT NULL COMMENT '1:有效, 0:删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员';
